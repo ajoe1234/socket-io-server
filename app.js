e3737377bdef50ba248abfcc11b560fb9d23c823
Wanchalerm <WANCHALERM.POPEE.Contractor@3ds.com>
2018-03-26 17:29:47 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+const app = express();
+app.use(index);
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on("connection", socket => {
+  console.log("New client connected"), setInterval(
+    () => getApiAndEmit(socket),
+    10000
+  );
+  socket.on("disconnect", () => console.log("Client disconnected"));
+});
+const getApiAndEmit = async socket => {
+  try {
+    const res = await axios.get(
+      "https://wpe1.net/api/argy/v1/history"
+    );
+    socket.emit("FromAPI", res.data[0]);
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+};
+server.listen(port, () => console.log(`Listening on port ${port}`));
