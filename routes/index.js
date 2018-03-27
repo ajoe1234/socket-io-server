006637881cad515b0e6e16438ae4a19298b3e918
Wanchalerm <WANCHALERM.POPEE.Contractor@3ds.com>
2018-03-27 11:04:56 +0900

    Added cors

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const cors = require('cors')
 const router = express.Router();
-router.get("/", (req, res) => {
+router.get("/",  cors(), (req, res) => {
   res.send({ response: "socket-io-server alive" }).status(200);
 });
 module.exports = router;
